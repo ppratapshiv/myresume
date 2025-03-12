9773b543e382ed531106afa5d4042f9c6098d140
ppratapshiv <ppratap.shirley@gmail.com>
2025-03-12 08:18:07 +0000

    added scripts

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3030},
+  base: '/myresume/', 
+  build: {
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+  },
+  server: {
+    port: 3030, 
+  },
   preview: {
     port: 7806, 
-  }
-})
+  },
+});
